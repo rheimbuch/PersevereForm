57da186a347c46eb92d76454ae34bad9580fbbea
Ryan Heimbuch <Ryan Heimbuch>
2009-05-04 12:51:00 -0600

    changes to allow yogo.persevere.Form to enable/disable auto updating the form/store when changes occur in one or the other. By default, auto updating functionality is enabled.

--- persevere/Form.js
+++ persevere/Form.js
@@ -5,6 +5,8 @@ dojo.require('yogo.schema.Form');
 dojo.declare('yogo.persevere.Form', yogo.schema.Form, {
     store: null,
     item: null,
+    updateStoreOnChange: true,
+    updateFormOnStoreChange: true,
     _setStoreAttr: function(store){
         if(store.schema) { 
             this.store = store;
@@ -17,7 +19,7 @@ dojo.declare('yogo.persevere.Form', yogo.schema.Form, {
         this._updateFormFromItem();
     },
     _onStoreItemUpdate: function(item, attribute, oldValue, newValue) {
-        if(this.item.id == item.id){
+        if(this.updateFormOnStoreChange && (this.item.id == item.id)){
             this._updateFormFromItem();
         }
     },
@@ -25,10 +27,23 @@ dojo.declare('yogo.persevere.Form', yogo.schema.Form, {
         this.attr('jsonValue', this.item);
     },
     onChange: function(value){
+        if(this.item && this.updateStoreOnChange){
+            // for(var name in value){
+            //     this.store.setValue(this.item, name, value[name]);
+            // }
+            this.save();
+        }
+    },
+    save: function() {
+        if(!this.store) { return; }
         if(this.item){
+            var value = this.attr('jsonValue');
             for(var name in value){
                 this.store.setValue(this.item, name, value[name]);
             }
+        }
+        else {
+            this.attr('item', this.store.newItem(this.attr('jsonValue')));
         }
     }
 });
